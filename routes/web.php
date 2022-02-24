cd5fd7de048d242ea7c79dc65997caa2e9ba4fa3
Kunal Rajput <kunal.rajput2511@gmail.com>
2022-02-24 21:04:31 +0530

    created rabbitm queuing system in laravel

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Jobs\LogSomething;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    dispatch(new LogSomething);
+    return view('welcome');
+});
